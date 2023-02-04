9b7550c49d449df5cebd7d92695486334eabb25d
riandkbayui <riandkbayui@gmail.com>
2023-02-04 13:22:59 +0700

    change database connection

--- src/configs/database.js
+++ src/configs/database.js
@@ -3,22 +3,22 @@ import path from "path";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-// const db = new Sequelize(
-//   "mvc-admin",
-//   "root",
-//   "",
-//   {
-//     host: '127.0.0.1',
-//     port: '3306',
-//     dialect: 'mysql',
-//     timezone: "+07:00",
-//     logging: false
-//   }
-// );
+const db = new Sequelize(
+  "sql12595615",
+  "sql12595615",
+  "mYr2rwWAvW",
+  {
+    host: 'sql12.freemysqlhosting.net',
+    port: '3306',
+    dialect: 'mysql',
+    timezone: "+07:00",
+    logging: false
+  }
+);
 
-const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: './src/configs/database.db'
-});
+// const db = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: './src/configs/database.db'
+// });
 
 export default db;
