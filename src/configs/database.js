b3a56b487e29f9ff63df2cea7a5038e1948f405f
riandkbayui <riandkbayui@gmail.com>
2023-02-10 10:33:51 +0700

    change database connection to sqlite again and add message test

--- src/configs/database.js
+++ src/configs/database.js
@@ -3,17 +3,23 @@ const path = require("path");
 
 // const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-const db = new Sequelize(
-  "sakurata_wa", //database name
-  "sakurata", // username
-  "gjm456PTW789", // password
-  {
-    host: 'sakuratadevapp.my.id',
-    port: '3306',
-    dialect: 'mysql',
-    timezone: "+07:00",
-    logging: false
-  }
-);
+// const db = new Sequelize(
+//   "sakurata_wa", //database name
+//   "sakurata", // username
+//   "gjm456PTW789", // password
+//   {
+//     host: 'sakuratadevapp.my.id',
+//     port: '3306',
+//     dialect: 'mysql',
+//     timezone: "+07:00",
+//     logging: false
+//   }
+// );
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: './src/configs/database.db',
+  logging: false
+});
 
 module.exports = db;
